eaee12899eff3354822cdd40b3e62cff3acbd0a7
Hyo Gyeong <cdkrcd8@gmail.com>
2022-04-12 21:26:30 +0900

    Editing...

--- concert_halls/views.py
+++ concert_halls/views.py
@@ -2,13 +2,16 @@ from rest_framework import viewsets, mixins, generics
 from .serializers import *
 from .models import ConcertHall, SeatArea
 from django.db.models import Prefetch
+from seat_reviews.models import Review
 
 
 class IndexViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = ConcertHallNameReviewImageSerializer
-    queryset = ConcertHall.objects.all().prefetch_related(
-        Prefetch("seat_areas",
-                 queryset=SeatArea.objects.prefetch_related("reviews").filter(reviews__images__len__gt=0).all())).all()
+    concert_hall = ConcertHall.objects.all().prefetch_related(
+        Prefetch('seat_areas',
+                 queryset=SeatArea.objects.prefetch_related('reviews').filter(reviews__images__len__gt=0).all())).all()
+    images = Review.objects.all()
+    queryset = chain(concert_hall, review)
 
 
 class SeatLayoutViewSet(generics.ListAPIView,
@@ -18,5 +21,5 @@ class SeatLayoutViewSet(generics.ListAPIView,
     def get_queryset(self):
         concert_hall_id = self.kwargs['concert_hall_id']
         queryset = ConcertHall.objects.filter(id=concert_hall_id).prefetch_related(
-            Prefetch("seat_areas", queryset=SeatArea.objects.prefetch_related("reviews").all())).all()
+            Prefetch('seat_areas', queryset=SeatArea.objects.prefetch_related('reviews').all())).all()
         return queryset
