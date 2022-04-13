1f1fd7a0ae680a4da52347c6b9d67b3a470479af
kuri-thevc <60633084+ku-ri@users.noreply.github.com>
2022-04-14 00:30:24 +0900

    Implement : settings.py dev랑 prod 분리 ( db 수정)

--- seeyaArchive/settings/development.py
+++ seeyaArchive/settings/development.py
@@ -7,8 +7,8 @@ ALLOWED_HOSTS = []
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'seeyadb_dev',
-        'USER': 'baggyuli',
+        'NAME': 'seeyadb',
+        'USER': 'postgres',
         'PASSWORD': 'seeyaarchive',
         'HOST': '127.0.0.1',
         'PORT': '5432',
