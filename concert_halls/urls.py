455778b8ffd83c07a364a005e6b1a957ef8aab89
Hyo Gyeong <cdkrcd8@gmail.com>
2022-04-16 15:55:07 +0900

    Rename: concert_hall_router

--- concert_halls/urls.py
+++ concert_halls/urls.py
@@ -2,14 +2,14 @@ from django.urls import include, path
 from rest_framework import routers
 from . import views
 
-router = routers.SimpleRouter(trailing_slash=False)
-router.register(r'concert_halls', views.ConcertHallViewSet, basename='concert_hall')
+concert_hall_router = routers.SimpleRouter(trailing_slash=False)
+concert_hall_router.register(r'concert_halls', views.ConcertHallViewSet, basename='concert_hall')
 
 seat_area_router = routers.SimpleRouter(trailing_slash=False)
 seat_area_router.register(r'seat_areas', views.SeatAreaViewSet, basename='seat_area')
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('', include(concert_hall_router.urls)),
     path('', views.SeatAreaViewSet.as_view({'get': 'retrieve'})),
     path('concert_halls/<int:concert_hall_id>/', include(seat_area_router.urls)),
 ]
