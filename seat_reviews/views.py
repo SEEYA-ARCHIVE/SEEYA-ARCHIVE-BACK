ccb602babd88b6649bce9b6e6182b56c0e59f2ad
kuri-thevc <60633084+ku-ri@users.noreply.github.com>
2022-04-24 22:10:00 +0900

    etc : 주석 제거

--- seat_reviews/views.py
+++ seat_reviews/views.py
@@ -29,20 +29,3 @@ class DetailReview(RetrieveAPIView):
     def get_queryset(self):
         return self.queryset.select_related('seat_area').filter(seat_area_id=self.kwargs['seat_area_id'])
 
-    # def retrieve(self, request, *args, **kwargs):
-    #     instance = self.get_object()
-    #     serializer = self.get_serializer(instance)
-    #
-    #     if instance == self.queryset.order_by("id").last():
-    #         next_id = None
-    #     else:
-    #         next_id = self.queryset.filter(id__lt=instance.id).order_by("id").last()
-    #
-    #     if instance == self.queryset.order_by("id").first():
-    #         previous_id = None
-    #     else:
-    #         previous_id = self.queryset.filter(id__lt=instance.id).order_by("id").first()
-    #     serializer.data['next_id'] = next_id
-    #     serializer.data['previous_id'] = previous_id
-    #
-    #     return Response(serializer.data)
