e3f6a79a8d7fd662b9c1f7cf1a573c4bc17d46b3
kuri-thevc <60633084+ku-ri@users.noreply.github.com>
2022-04-26 20:09:58 +0900

    fix : requirement 수정

--- concert_halls/views.py
+++ concert_halls/views.py
@@ -1,6 +1,4 @@
-from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
-from rest_framework.response import Response
 
 from .serializers import *
 from .models import ConcertHall, SeatArea
