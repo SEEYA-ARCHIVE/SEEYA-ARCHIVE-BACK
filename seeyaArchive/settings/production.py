dce131b7527cb2d041b703aaa818106baf76e99a
kuri-thevc <60633084+ku-ri@users.noreply.github.com>
2022-05-03 23:55:52 +0900

    fix : 설정 변경

--- seeyaArchive/settings/production.py
+++ seeyaArchive/settings/production.py
@@ -31,7 +31,7 @@ STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-
+AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 MEDIA_URL = 'https://%s/' % (AWS_S3_CUSTOM_DOMAIN)
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
