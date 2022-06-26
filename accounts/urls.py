167fe37eedcbcc02b197fb529c8fdccf4ef63fd4
Hyo Gyeong <cdkrcd8@gmail.com>
2022-06-27 02:53:23 +0900

    implement: kakao login

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,12 +2,11 @@ from django.urls import include, path
 from rest_framework import routers
 from . import views
 
-# account_router = routers.SimpleRouter(trailing_slash=False)
-# account_router.register('accounts', views.getUserInfo, basename='account')
 
 urlpatterns = [
     path('kakao_login/', views.kakao_login),
     path('kakao_logout/', views.kakao_logout),
-    # path('logout/', views.logout),
+    path('kakao_secession/', views.kakao_secession),
     path('accounts/kakao/login/callback/', views.kakao_login_callback),
+    path('me/', views.SetNicknameView.as_view()),
 ]
--- seat_reviews/serializers.py
+++ seat_reviews/serializers.py
@@ -1,12 +1,22 @@
 from django.conf import settings
 from rest_framework.serializers import ModelSerializer
-from .models import Review
+from .models import Review, Comment
+from accounts.serializers import UserSerializer
 from rest_framework import serializers
 import os
 
 
+class CommentSerializer(ModelSerializer):
+    user = UserSerializer
+
+    class Meta:
+        model = Comment
+        fields = ['user', 'id', 'comment', 'create_at', 'update_at']
+
+
 class SeatReviewsSerializer(ModelSerializer):
     images = serializers.SerializerMethodField()
+    comments = CommentSerializer(many=True, read_only=True)
 
     def get_images(self, obj):
         return {'preview_image': os.path.join(settings.MEDIA_URL, 'review-images', obj.images[0]),
@@ -14,7 +24,7 @@ class SeatReviewsSerializer(ModelSerializer):
 
     class Meta:
         model = Review
-        fields = ['id', 'create_at', 'images']
+        fields = ['id', 'create_at', 'images', 'comments']
 
 
 class ReviewSerializer(ModelSerializer):
