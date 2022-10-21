57f02172dcc21d19f1ef1590e851e4e8cfb431d7
parkkuri <kebee19@naver.com>
2022-10-21 15:29:57 +0900

    fix : static file 경로 수정

--- seeyaArchive/settings/production.py
+++ seeyaArchive/settings/production.py
@@ -62,7 +62,7 @@ AWS_S3_CUSTOM_DOMAIN = "%s.s3.%s.amazonaws.com" % (AWS_STORAGE_BUCKET_NAME, AWS_
 STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
 STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
