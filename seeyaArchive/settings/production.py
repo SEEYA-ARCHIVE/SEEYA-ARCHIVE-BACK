14ecd78fb45a46479a146d33daa43f010b3754e7
parkkuri <kebee19@naver.com>
2022-10-22 14:17:07 +0900

    fix : static file 경로 바꿔서 배포

--- seeyaArchive/settings/production.py
+++ seeyaArchive/settings/production.py
@@ -64,7 +64,7 @@ AWS_S3_CUSTOM_DOMAIN = "%s.s3.%s.amazonaws.com" % (AWS_STORAGE_BUCKET_NAME, AWS_
 STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
 STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = "static"
 
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
