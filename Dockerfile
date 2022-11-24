e62786bd6ab6d89b9668dc1249fbfa4a36c2b822
Hyo Gyeong <cdkrcd8@gmail.com>
2022-11-24 20:56:18 +0900

    Add: pull develop branch

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,17 @@
-
 FROM python:3.9.0
 
 RUN python -m pip install --upgrade pip
 
-WORKDIR /home
+COPY . /app
 
-RUN git clone -b develop --single-branch https://github.com/SEEYA-ARCHIVE/SEEYA-ARCHIVE-BACK.git
-
-WORKDIR /home/SEEYA-ARCHIVE-BACK
+WORKDIR /app
 
 RUN pip install -r requirements.txt
 
 RUN pip install gunicorn==20.1.0
 
-EXPOSE 80
+ENV ENVIRONMENT prod
 
-ENTRYPOINT ["/bin/sh", "-c" , "cd /home/SEEYA-ARCHIVE-BACK && python manage.py collectstatic --noinput --settings=seeyaArchive.settings.production && python manage.py migrate --settings=seeyaArchive.settings.production && gunicorn seeyaArchive.wsgi:application --bind 0.0.0.0:80"]
+EXPOSE 8000
+
+ENTRYPOINT ["/bin/sh", "-c" , "python manage.py collectstatic --noinput --settings=seeyaArchive.settings.production && python manage.py migrate --settings=seeyaArchive.settings.production && gunicorn seeyaArchive.wsgi:application --bind 0.0.0.0:8000"]
--- seat_reviews/serializers.py
+++ seat_reviews/serializers.py
@@ -4,7 +4,11 @@ from datetime import datetime
 from accounts.models import User
 from django.conf import settings
 from rest_framework.exceptions import APIException
-from rest_framework.serializers import ModelSerializer, Serializer, SerializerMethodField
+from rest_framework.serializers import (
+    ModelSerializer,
+    Serializer,
+    SerializerMethodField,
+)
 from .models import Review, Comment
 
 if settings.DEBUG:
@@ -12,12 +16,11 @@ if settings.DEBUG:
 else:
     from seeyaArchive.settings.production import SOCIAL_OAUTH_CONFIG
 
-## AWS
-
+# AWS
 AWS_ACCESS_KEY_ID = SOCIAL_OAUTH_CONFIG['MY_AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = SOCIAL_OAUTH_CONFIG['MY_AWS_SECRET_ACCESS_KEY']
 AWS_REGION = 'ap-northeast-2'
-AWS_STORAGE_BUCKET_NAME = '7th-team2-seeya-archive'
+AWS_STORAGE_BUCKET_NAME = 'seeya-archive'
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.%s.amazonaws.com' % (AWS_STORAGE_BUCKET_NAME, AWS_REGION)
 
 
@@ -71,7 +74,7 @@ class CommentSerializer(ModelSerializer):
 
 class SeatReviewImageUploadS3Serializer(Serializer):
     def to_representation(self, image_url_list):
-        image_dict = {'image_urls': image_url_list}  # 수정 필요
+        image_dict = {'image_urls': image_url_list}
         return image_dict
 
     def create(self, validate_data):
@@ -82,19 +85,27 @@ class SeatReviewImageUploadS3Serializer(Serializer):
         if len(images_data.getlist('image')) == 0:
             raise ImageRequiredException
 
-        s3r = boto3.resource('s3', aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+        s3r = boto3.resource(
+            's3',
+            aws_access_key_id=AWS_ACCESS_KEY_ID,
+            aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+        )
         bucket_name = 'review-images'
         current_date = datetime.now().strftime('%Y_%m_%d-%H:%M:%S')
 
         image_url_list = []
         for image_data in images_data.getlist('image'):
-            image_data._set_name(str(uuid.uuid4())) # use uuid as temporary name
-            # s3에 이미지 저장
-            s3r.Bucket(AWS_STORAGE_BUCKET_NAME).put_object(Key='%s/%s-%s' % (bucket_name, current_date, image_data),
-                                                           Body=image_data, ContentType='jpg')
-            # 이미지 url 리스트 생성
+            image_data._set_name(str(uuid.uuid4()))
+            s3r.Bucket(AWS_STORAGE_BUCKET_NAME).put_object(
+                Key='%s/%s-%s' % (bucket_name, current_date, image_data),
+                Body=image_data,
+                ContentType='jpg',
+            )
             image_url_list.append(
-                'https://' + AWS_S3_CUSTOM_DOMAIN + '/%s/%s-%s' % (bucket_name, current_date, image_data))
+                'https://'
+                + AWS_S3_CUSTOM_DOMAIN
+                + '/%s/%s-%s' % (bucket_name, current_date, image_data)
+            )
         return self.to_representation(image_url_list)
 
 
@@ -106,8 +117,16 @@ class SeatReviewListSerializer(ModelSerializer):
 
     class Meta:
         model = Review
-        fields = ['id', 'seat_area', 'user', 'nickname', 'review', 'like_users', 'preview_image',
-                  'image_url_array']
+        fields = [
+            'id',
+            'seat_area',
+            'user',
+            'nickname',
+            'review',
+            'like_users',
+            'preview_image',
+            'image_url_array',
+        ]
         read_only_fields = ['id']
 
     def get_preview_image(self, obj): # DB상 첫 번째 이미지가 preview image
@@ -126,7 +145,13 @@ class SeatReviewListSerializer(ModelSerializer):
 class SeatReviewCreateSerializer(ModelSerializer):
     class Meta:
         model = Review
-        fields = ['id', 'seat_area', 'user', 'image_url_array', 'review', 'create_at']
+        fields = [
+            'id',
+            'seat_area',
+            'user',
+            'image_url_array',
+            'review',
+            'create_at']
         read_only_fields = ['id']
 
 
@@ -139,8 +164,18 @@ class SeatReviewDetailSerializer(ModelSerializer):
 
     class Meta:
         model = Review
-        fields = ['id', 'user', 'seat_area', 'concert_hall_name', 'image_url_array', 'create_at',
-                  'update_at', 'review', 'review_comments', 'like_users']
+        fields = [
+            'id',
+            'user',
+            'seat_area',
+            'concert_hall_name',
+            'image_url_array',
+            'create_at',
+            'update_at',
+            'review',
+            'review_comments',
+            'like_users'
+        ]
         read_only_fields = ['id']
 
     def get_seat_area(self, obj):
@@ -159,8 +194,16 @@ class ViewComparisonSerializer(ModelSerializer):
 
     class Meta:
         model = Review
-        fields = ['id', 'user_nickname', 'thumbnail_image', 'seat_area_name', 'review',
-                  'create_at', 'count_like_users', 'count_comments']
+        fields = [
+            'id',
+            'user_nickname',
+            'thumbnail_image',
+            'seat_area_name',
+            'review',
+            'create_at',
+            'count_like_users',
+            'count_comments',
+        ]
         read_only_fields = ['id']
 
     def get_count_like_users(self, obj):
