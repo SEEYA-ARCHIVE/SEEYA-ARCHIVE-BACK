93d84e000e699774a2f743b004ad3f83363620f1
Hyo Gyeong <cdkrcd8@gmail.com>
2022-11-26 17:11:14 +0900

    add: json api

--- concert_halls/views.py
+++ concert_halls/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import redirect
 from rest_framework import viewsets
 from .serializers import *
 from .models import ConcertHall, SeatArea
@@ -15,3 +16,15 @@ class SeatAreaViewSet(viewsets.ReadOnlyModelViewSet):
         concert_hall_id = self.kwargs['concert_hall_id']
         queryset = SeatArea.objects.filter(concert_hall_id=concert_hall_id).all()
         return queryset
+
+
+def mini_seat_olympic(request):
+    return redirect('https://q07w2g2l9e.execute-api.ap-northeast-2.amazonaws.com/seeya/mini-seat-olympic')
+
+
+def seat_olympic(request):
+    return redirect('https://q07w2g2l9e.execute-api.ap-northeast-2.amazonaws.com/seeya/seat-olympic')
+
+
+def upload_olympic(request):
+    return redirect('https://q07w2g2l9e.execute-api.ap-northeast-2.amazonaws.com/seeya/upload-olympic')
